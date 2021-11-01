1ada1b1c85b36d8676741bb3fc0426c0187ba52c
Sam <sam.pan@itrustmachines.com>
2021-11-01 14:40:19 +0800

    init

---
+++ ITM.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+import "../node_modules/@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+contract ITM is ERC721PresetMinterPauserAutoId {
+    constructor() public
+    ERC721PresetMinterPauserAutoId("ITrustMachines", "ITM", "https://www.frank.hk/token/")  
+    {}
+    // This allows the minter to update the tokenURI after it's been minted.
+    // To disable this, delete this function.
+    function setTokenURI(uint256 tokenId, string memory tokenURI) public {
+        require(hasRole(MINTER_ROLE, _msgSender()), "web3 CLI: must have minter role to update tokenURI");
+        setTokenURI(tokenId, tokenURI);
+    }
+}
